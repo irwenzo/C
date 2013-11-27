aaf0402d9e4b01058d0fc5e5fdf504071b28e4ac
irwenzo <margusn@msn.com>
2013-11-27 22:59:58 +0200

    korrutustabel.c

---
+++ korrutustabel.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, char*argv[]) 
+	{
+	int i,j,k,l,t;
+	for (i=01;i<=05;i++){
+		printf("%02d %02d %02d %02d %02d\n",i,j,k,l,t,j=i+i,k=i*3, l=i*4, t=i*5);
+		}	
+	return 0;
+}
+
+
